1a2256e0c79bd1b4ff6348b0c0697cbf6b72033a
George Ikigu <ikigu.geo@gmail.com>
2024-07-23 20:12:45 +0300

    solution: task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  Promise.all([photoPromise, userPromise])
+    .then(([photoResponse, userResponse]) => {
+      console.log(
+        `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
