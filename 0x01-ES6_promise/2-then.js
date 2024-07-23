c55bd5711790268013e06ce530637aefa8fa2da4
George Ikigu <ikigu.geo@gmail.com>
2024-07-23 20:04:18 +0300

    fix: _

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      // When the Promise resolves successfully
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' }; // Return the object
-    })
-    .catch(() => {
-      return new Error();
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
